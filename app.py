6ab33a67b79117c22c5944b57de31c13f8b0466f
oleeper-hue <oleeper@asu.edu>
2025-11-27 11:49:32 -0800

    Update app.py

--- app.py
+++ app.py
@@ -138,11 +138,8 @@ goal_weight_kg = num_slider_float("Weight (kg)", 2, 1, 3, step=0.01, id='goal')
 desired_timeline_weeks = st.slider("Weeks to Reach Goal Weight", min_value=4, max_value=104, value=16, step=1)
 
 #if (goal_weight_kg and desired_timeline_weeks and step_1_completed):
-if st.button("Calculate Calorie Intake for Timeline"):
+if (st.button("Calculate Calorie Intake for Timeline") and step_1_completed):
     try:
-        if not step_1_completed:
-            st.error('Please complete step 1 before step 2')
-            continue
         pred = best_model.predict(new_user_df)[0]
         if new_user_df['Gender'].iloc[0] == 'Male':
             bmr = 88.362 + (13.397 * new_user_df['Weight (kg)'].iloc[0]) + (4.799 * new_user_df['Height (m)'].iloc[0] * 100) - (5.677 * new_user_df['Age'].iloc[0])
