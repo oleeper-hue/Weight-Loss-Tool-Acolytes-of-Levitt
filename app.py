7f877e9721da50aaf11810588742c7a223b7bf31
oleeper-hue <oleeper@asu.edu>
2025-11-27 11:35:07 -0800

    Update app.py

--- app.py
+++ app.py
@@ -116,7 +116,6 @@ new_user_df = pd.DataFrame([new_user])
 st.divider()
 if st.button("Predict"):
     try:
-        global pred
         pred = best_model.predict(new_user_df)[0]
 
         st.subheader("Prediction Result")
@@ -140,6 +139,7 @@ desired_timeline_weeks = st.slider("Weeks to Reach Goal Weight", min_value=4, ma
 #if (goal_weight_kg and desired_timeline_weeks and step_1_completed):
 if st.button("Calculate Calorie Intake for Timeline"):
     try:
+        pred = best_model.predict(new_user_df)[0]
         if new_user_df['Gender'].iloc[0] == 'Male':
             bmr = 88.362 + (13.397 * new_user_df['Weight (kg)'].iloc[0]) + (4.799 * new_user_df['Height (m)'].iloc[0] * 100) - (5.677 * new_user_df['Age'].iloc[0])
         elif new_user_df['Gender'].iloc[0] == 'Female':
